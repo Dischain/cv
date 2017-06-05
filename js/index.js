f42b02f859d182c7dc305ad9b4b8a45b87fede48
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-06-05 23:40:05 +0300

    removed some ugly stuff

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $(target).offset().top}, 800);
     });
 
-    $(window).scroll(() => {
+    $(window).scroll(function() {
         if($(this).scrollTop() != 0) {
             $('.scroll-to-top').fadeIn();
         } else {
@@ -13,24 +13,7 @@ $(document).ready(function() {
         }
     });
 
-    $('.scroll-to-top').click(() => {
+    $('.scroll-to-top').click(function() {
         $('body,html').animate({scrollTop:0},800);
-    });
-
-    $(window).scroll(() => {
-    	function elementScrolled(elem) {
-	        var docViewTop = $(window).scrollTop();
-	        var docViewBottom = docViewTop + $(window).height();
-	        var elemTop = $(elem).offset().top;
-	        return ((elemTop <= docViewBottom - 100) && (elemTop >= docViewTop));
-    	}
-    	$('h2[id^=anchor_]').each(function(){
-    		let p = $('a[href="#' + $(this).attr('id') + '"]').parent();
-    		if (elementScrolled($(this))) {
-    			p.addClass('side-bar__item_active');
-    		} else {
-    			p.removeClass('side-bar__item_active')
-    		}
-    	})
     });
 });
